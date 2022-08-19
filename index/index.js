599a3745d3e4a71046f1682621a0e7058f7eb16b
Douglas Volcato <douglasvolcato@gmail.com>
2022-08-18 23:41:21 -0300

    Update

--- index/index.js
+++ index/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('../src/routes/termo.route');
-const connectToDatabase = require('../src/database/database');
+// const routes = require('../src/routes/termo.route');
+// const connectToDatabase = require('../src/database/database');
 
 const port = 3000;
 const app = express();
 
-connectToDatabase();
+// connectToDatabase();
 
 app.use(express.json());
 app.use(cors());
