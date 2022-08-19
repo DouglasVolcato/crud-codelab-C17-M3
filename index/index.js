0b526b5eb52cb3a461b0642166053e212cbff237
Douglas Volcato <douglasvolcato@gmail.com>
2022-08-18 23:54:13 -0300

    Update

--- index/index.js
+++ index/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-// const routes = require('../src/routes/termo.route');
-// const connectToDatabase = require('../src/database/database');
+const routes = require('../src/routes/termo.route');
+const connectToDatabase = require('../src/database/database');
 const app = express();
 
-// connectToDatabase();
+connectToDatabase();
 
 app.use(express.json());
 app.use(cors());
-app.get("/", (req,res) => res.send({message: "Hello World"}))
-// app.use('/termos', routes);
-// app.use('/api-docs', routes);
+app.use('/termos', routes);
+app.use('/api-docs', routes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
